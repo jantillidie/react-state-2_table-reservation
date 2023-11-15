5c03a007bca1a1adbda451cdee1e1c6df897afa7
Jan Maaß <git@antipost.de>
2023-11-15 16:42:59 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+  function handlePlusClick() {
+    setPeople(people + 1);
+  }
+  function handleMinusClick() {
+    setPeople(people - 1);
+  }
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter plusClick={handlePlusClick} minusClick={handleMinusClick} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
